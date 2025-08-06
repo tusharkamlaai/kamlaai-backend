76e86189265906d13d147741aab4d846a8665250
tushar <tushar.kamlaai@gmail.com>
2025-08-06 11:06:16 +0530

    f

--- config/db.js
+++ config/db.js
@@ -1,22 +1,8 @@
-// const { createClient } = require('@supabase/supabase-js');
+const { createClient } = require('@supabase/supabase-js');
 
-// const supabaseUrl = process.env.SUPABASE_URL;
-// const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 
-// const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseKey);
 
-// module.exports = supabase;
-
-
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY,
-  {
-    db: {
-      schema: 'public' // Explicitly set schema
-    },
-    auth: {
-      persistSession: true
-    }
-  }
-);
+module.exports = supabase;
