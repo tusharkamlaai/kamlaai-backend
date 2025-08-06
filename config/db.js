71fda46fa80a67b0fc1240ef41501faceadb5be4
tushar <tushar.kamlaai@gmail.com>
2025-08-06 11:34:23 +0530

    e

--- config/db.js
+++ config/db.js
@@ -1,32 +1,8 @@
 const { createClient } = require('@supabase/supabase-js');
 
-// Check if environment variables exist
-if (!process.env.SUPABASE_URL || !process.env.SUPABASE_KEY) {
-  throw new Error(
-    'Supabase URL and Key must be provided in environment variables'
-  );
-}
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY,
-  {
-    auth: {
-      persistSession: false // Recommended for server-side usage
-    }
-  }
-);
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 module.exports = supabase;
-
-
-
-
-// const { createClient } = require('@supabase/supabase-js');
-
-// const supabaseUrl = process.env.SUPABASE_URL;
-// const supabaseKey = process.env.SUPABASE_KEY;
-
-// const supabase = createClient(supabaseUrl, supabaseKey);
-
-// module.exports = supabase;
